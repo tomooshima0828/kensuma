4ae36d107d84c7d65abf9b0a03724be4f250e97e
yusamasaki <masa.aw1208@gmail.com>
2022-01-08 01:55:22 +0900

    businessモデルバリデーション追加

--- app/models/business.rb
+++ app/models/business.rb
@@ -1,3 +1,18 @@
 class Business < ApplicationRecord
+  before_create -> { self.uuid = SecureRandom.uuid }
+
   belongs_to :user
+
+  validates :uuid, presence: true
+  validates :name, presence: true
+  validates :name_kana, presence: true
+  validates :branch_name, presence: true
+  validates :representative_name, presence: true
+  validates :email, presence: true
+  validates :address, presence: true
+  validates :post_code, presence: true
+  validates :phone_number, presence: true
+  validates :business_type, presence: true
+
+  enum business_type: { corporation: 0, freelance: 1, Individual_five_over: 2, Individual_five_less: 3 }
 end
