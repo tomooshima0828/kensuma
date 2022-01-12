94434d94d91b66442a9f6e3f0c610aa8183b2663
RumiNakamura <krw.psh2196@gmail.com>
2022-01-12 21:08:05 +0900

    ユーザー情報編集機能周りの修正、systemspecの設定追加

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -5,7 +5,7 @@ ActiveAdmin.register User do
   #
   # Uncomment all parameters which should be permitted for assignment
   #
-  permit_params :email, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at, :sign_in_count, :current_sign_in_at, :last_sign_in_at, :current_sign_in_ip, :last_sign_in_ip, :confirmation_token, :confirmed_at, :confirmation_sent_at, :unconfirmed_email, :failed_attempts, :unlock_token, :locked_at, :name, :age, :gender
+  permit_params :email, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at, :sign_in_count, :current_sign_in_at, :last_sign_in_at, :current_sign_in_ip, :last_sign_in_ip, :confirmation_token, :confirmed_at, :confirmation_sent_at, :unconfirmed_email, :failed_attempts, :unlock_token, :locked_at, :name, :age, :gender, :role
   #
   # or
   #
@@ -16,13 +16,42 @@ ActiveAdmin.register User do
   # end
 
   index do
-    column :email
     column :name
+    column :email
     column :age
     column :gender
+    column :role
 
-    actions # 後述するpermit_paramsの設定に応じて閲覧編集削除などのリンクを表示します
+    # 閲覧編集削除などのリンク表示
+    actions
   end
+
+  filter :email
+  filter :age
+  filter :gender
+  filter :role
   
-  
+  show do
+    attributes_table do
+      row :name
+      row :email
+      row :age
+      row :gender
+      row :role
+    end
+  end
+
+  form do |f|
+    f.inputs do
+      f.input :name
+      f.input :email
+      f.input :age
+      f.input :gender
+      f.input :role
+      f.input :password
+      f.input :password_confirmation
+    end
+    f.actions
+  end
+
 end
