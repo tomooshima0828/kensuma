cf086e498193b93f02a58a1eb7433d031f7260ef
yusamasaki <masa.aw1208@gmail.com>
2022-01-15 18:30:54 +0900

    business controller整理・コメントアウト追加

--- app/controllers/users/businesses_controller.rb
+++ app/controllers/users/businesses_controller.rb
@@ -1,11 +1,10 @@
 module Users
   class BusinessesController < Users::Base
-
     before_action :set_business, except: [:new, :create]
-    before_action :business_no_access, only: [:new]
+    before_action :business_no_access, only: :new
 
     def new
-      @business = Business.new( uuid: "test", name: "test", name_kana: "テスト", branch_name: "test", representative_name: "test", email: "test@email.com", 
+      @business = Business.new( uuid: "1", name: "test企業", name_kana: "テストキギョウ", branch_name: "test", representative_name: "test支店", email: "test@email.com", 
                                 address: "test", post_code: "0123456", phone_number: "01234567898", carrier_up_id: "test", business_type: 0, user_id: current_user.id)
     end
 
@@ -33,28 +32,19 @@ module Users
     def show
     end
     
-    def stamp_images_add
-      stamp_images = @business.stamp_images 
-      stamp_images + new_stamp_images
-      @business.stamp_images = images
-      flash[:error] = "Failed uploading images" unless @gallery.save
-      redirect_to :back
-    end
-
-
     def stamp_images_delete
+      # 残りstamp_imageを定義
       remain_stamp_images = @business.stamp_images
-
+      # stamp_imageを削除する
       deleted_stamp_image = remain_stamp_images.delete_at(params[:index].to_i)
       deleted_stamp_image.try(:remove!)
+      # 削除した後のstamp_imageをupdateする
       @business.update!(stamp_images: remain_stamp_images)
-
       flash[:danger] = "削除しました"
       redirect_to edit_users_businesses_path
     end
 
     private
-
       def set_business
         @business =  Business.find_by(user_id: current_user.id)
       end
@@ -62,6 +52,5 @@ module Users
       def business_params
         params.require(:business).permit(:uuid, :name, :name_kana, :branch_name, :representative_name, :email, :address, :post_code, :phone_number, :carrier_up_id, :business_type, { stamp_images: [] }, :user_id)
       end
-
   end
 end
