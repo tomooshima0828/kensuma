96357244a19f0c6cf4ab6547bb60cff6c1039bdb
yusamasaki <masa.aw1208@gmail.com>
2022-01-25 12:32:38 +0900

    general_usersコントローラー/作成

---
+++ app/controllers/users/general_users_controller.rb
@@ -0,0 +1,17 @@
+module Users
+  class GeneralUsersController < Users::Base
+    def index; end
+
+    def show; end
+
+    def new; end
+
+    def edit; end
+
+    def create; end
+
+    def update; end
+
+    def destroy; end
+  end
+end
