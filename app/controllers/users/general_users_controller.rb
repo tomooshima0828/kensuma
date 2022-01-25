8fb371904504268b3da2a6072e499ad1b3920530
yusamasaki <masa.aw1208@gmail.com>
2022-01-25 23:30:47 +0900

    show追加/index→show矢印editの推移/update機能追加

--- app/controllers/users/general_users_controller.rb
+++ app/controllers/users/general_users_controller.rb
@@ -1,5 +1,7 @@
 module Users
   class GeneralUsersController < Users::Base
+    before_action :set_general_user, except: %i[index new create]
+
     def index
       @admin = current_user
       @general_users = User.where(admin_user_id: @admin)
@@ -22,13 +24,25 @@ module Users
 
     def edit; end
 
-    def update; end
+    def update
+      if @general_user.update(general_user_params)
+        flash[:success] = '更新しました'
+        redirect_to users_general_user_url
+      else
+        render 'edit'
+      end
+    end
 
     def show; end
 
     def destroy; end
 
     private
+
+    def set_general_user
+      @admin = current_user
+      @general_user = @admin.general_users.find(params[:id])
+    end
 
     def general_user_params
       params.require(:user).permit(:name, :email, :age, :gender, :password)
