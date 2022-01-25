1e56d93319a94faf301cedb88c134d1c060a7286
yusamasaki <masa.aw1208@gmail.com>
2022-01-25 22:23:41 +0900

    general_usersを新規作成してindexに一覧表示。/日本語化

--- app/controllers/users/general_users_controller.rb
+++ app/controllers/users/general_users_controller.rb
@@ -1,17 +1,37 @@
 module Users
   class GeneralUsersController < Users::Base
-    def index; end
+    def index
+      @admin = current_user
+      @general_users = User.where(admin_user_id: @admin)
+    end
 
-    def show; end
+    def new
+      @admin = current_user
+      @general_user = @admin.general_users.new
+    end
 
-    def new; end
+    def create
+      @admin = current_user
+      @general_user = @admin.general_users.new(general_user_params)
+      if @general_user.save
+        redirect_to users_general_users_url
+      else
+        render :new
+      end
+    end
 
     def edit; end
 
-    def create; end
-
     def update; end
 
+    def show; end
+
     def destroy; end
+
+    private
+
+    def general_user_params
+      params.require(:user).permit(:name, :email, :age, :gender, :password)
+    end
   end
 end
