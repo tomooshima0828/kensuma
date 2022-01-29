830aee3b01f383198cb9e21d98a2b6b8dcc63a04
RumiNakamura <krw.psh2196@gmail.com>
2022-01-29 10:56:12 +0900

    Workerモデルのバリデーション、enum定義を追加

--- app/models/worker.rb
+++ app/models/worker.rb
@@ -1,3 +1,24 @@
 class Worker < ApplicationRecord
   belongs_to :businesses
+
+  enum abo_blood_type: { a: 0, b: 1, ab: 2, o: 3 }
+  enum rh_blood_type: { plus: 0, minus: 1 }
+  # 内訳未定のためコメントアウト
+  # enum job_type: {  }
+
+  validates :name,  presence: true
+  validates :name_kana,  presence: true
+  validates :country,  presence: true
+  validates :my_address,  presence: true
+  validates :my_phone_number,  presence: true
+  validates :family_address,  presence: true
+  validates :family_phone_number,  presence: true
+  validates :birth_day_on,  presence: true
+  validates :abo_blood_type,  presence: true
+  validates :rh_blood_type,  presence: true
+  validates :job_type,  presence: true
+  validates :hiring_on,  presence: true
+  validates :experience_term_before_hiring,  presence: true
+  validates :blank_term,  presence: true
 end
+
