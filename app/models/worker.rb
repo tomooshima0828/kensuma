801d51e4a98bf7c9e600c78d46c791a825c59e57
RumiNakamura <krw.psh2196@gmail.com>
2022-01-30 11:39:22 +0900

    Workerモデルの日本語訳を追加、ja.ymlを修正、カタカナ正規表現を追加

--- app/models/worker.rb
+++ app/models/worker.rb
@@ -6,18 +6,19 @@ class Worker < ApplicationRecord
   # ↓内訳未定のためコメントアウト
   # enum job_type: {  }
 
-  validates :name,  presence: true
-  validates :name_kana,  presence: true # カタカナであること
-  validates :country,  presence: true
-  validates :my_address,  presence: true
-  validates :my_phone_number,  presence: true
-  validates :family_address,  presence: true
-  validates :family_phone_number,  presence: true
-  validates :birth_day_on,  presence: true
-  validates :abo_blood_type,  presence: true
-  validates :rh_blood_type,  presence: true
-  validates :job_type,  presence: true
-  validates :hiring_on,  presence: true
-  validates :experience_term_before_hiring,  presence: true
-  validates :blank_term,  presence: true
+  VALID_NAME_KANA_REGEX = /\A[ァ-ヶー－]+\z/
+  validates :name, presence: true
+  validates :name_kana, presence: true, format: { with: VALID_NAME_KANA_REGEX }
+  validates :country, presence: true
+  validates :my_address, presence: true
+  validates :my_phone_number, presence: true
+  validates :family_address, presence: true
+  validates :family_phone_number, presence: true
+  validates :birth_day_on, presence: true
+  validates :abo_blood_type, presence: true
+  validates :rh_blood_type, presence: true
+  validates :job_type, presence: true
+  validates :hiring_on, presence: true
+  validates :experience_term_before_hiring, presence: true
+  validates :blank_term, presence: true
 end
