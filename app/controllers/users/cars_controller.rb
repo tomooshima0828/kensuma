adbd748021aa4e25909a36966bce2e4ad20f2e77
Tadakiyo-Hirano <d85nus03@gmaul.com>
2022-01-30 11:11:45 +0900

    add cars controller

---
+++ app/controllers/users/cars_controller.rb
@@ -0,0 +1,24 @@
+module Users
+  class CarsController < Users::Base
+    def index
+    end
+
+    def show
+    end
+
+    def new
+    end
+
+    def create
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -26,6 +26,7 @@ Rails.application.routes.draw do
   }
 
   namespace :users do
+    resources :cars
     resources :general_users
     resources :dash_boards, only: [:index]
     resources :articles, only: %i[index show]
