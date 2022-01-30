788dab5bf25fa043bd325148037736e15e495884
Tadakiyo-Hirano <d85nus03@gmaul.com>
2022-01-30 10:51:58 +0900

    add car validation

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,2 +1,10 @@
 class Car < ApplicationRecord
+  validates :owner_name, presence: true
+  validates :vehicle_model, presence: true
+  validates :vehicle_number, presence: true
+  validates :vehicle_inspection_start_on, presence: true
+  validates :vehicle_inspection_end_on, presence: true
+  validates :liability_securities_number, presence: true
+  validates :liability_insurance_start_on, presence: true
+  validates :liability_insurance_end_on, presence: true
 end
