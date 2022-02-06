4751221dbc7646955eded48344390874b8b62ef9
yusamasaki <masa.aw1208@gmail.com>
2022-02-07 05:52:46 +0900

    delete_accepts_nested_attributes_for

--- app/models/car_insurance_company.rb
+++ app/models/car_insurance_company.rb
@@ -5,8 +5,5 @@ class CarInsuranceCompany < ApplicationRecord
   has_many :car_voluntary_insurances, class_name: 'CarVoluntaryInsurance', foreign_key: :company_voluntary_id, dependent: :destroy
   has_many :car_voluntaries, through: :car_voluntary_insurances, source: :car_voluntary
 
-  accepts_nested_attributes_for :car_liability_insurances, allow_destroy: true
-  accepts_nested_attributes_for :car_voluntary_insurances, allow_destroy: true
-
   validates :name, presence: true
 end
