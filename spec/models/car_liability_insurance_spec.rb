2c36b76ddbe81750115b9d110aab7cb6bb99507e
yusamasaki <masa.aw1208@gmail.com>
2022-02-06 23:56:19 +0900

    add_liability_voluntary_spec

--- spec/models/car_liability_insurance_spec.rb
+++ spec/models/car_liability_insurance_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe CarLiabilityInsurance, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let :company do
+    create(:car_insurance_company)
+  end
+
+  let :car do
+    create(:car)
+  end
+
+  let :liability do
+    create(:car_liability_insurance, company_liability: company, car_liability: car)
+  end
+
+  describe 'アソシエーションについて' do
+    context '紐つく車両がある場合' do
+      subject do
+        liability.car_liability
+      end
+      it '紐つく車両を返すこと' do
+        expect(subject).to eq(car)
+      end
+    end
+
+    context '紐つく車両がある場合' do
+      subject do
+        liability.company_liability
+      end
+      it '紐つく車両を返すこと' do
+        expect(subject).to eq(company)
+      end
+    end
+  end
 end
--- spec/models/car_voluntary_insurance_spec.rb
+++ spec/models/car_voluntary_insurance_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe CarVoluntaryInsurance, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let :company do
+    create(:car_insurance_company)
+  end
+
+  let :car do
+    create(:car)
+  end
+
+  let :voluntary do
+    create(:car_voluntary_insurance, company_voluntary: company, car_voluntary: car)
+  end
+
+  describe 'アソシエーションについて' do
+    context '紐つく車両がある場合' do
+      subject do
+        voluntary.car_voluntary
+      end
+      it '紐つく車両を返すこと' do
+        expect(subject).to eq(car)
+      end
+    end
+
+    context '紐つく車両がある場合' do
+      subject do
+        voluntary.company_voluntary
+      end
+      it '紐つく車両を返すこと' do
+        expect(subject).to eq(company)
+      end
+    end
+  end
 end
