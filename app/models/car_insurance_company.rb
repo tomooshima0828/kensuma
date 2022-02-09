ae8f17c69abe2187c04e58342ac6a6490d0712b9
yusamasaki <masa.aw1208@gmail.com>
2022-02-09 22:26:34 +0900

    fix_association

--- app/models/car_insurance_company.rb
+++ app/models/car_insurance_company.rb
@@ -1,5 +1,5 @@
 class CarInsuranceCompany < ApplicationRecord
-  belongs_to :car
+  has_many :cars
   has_many :car_voluntary_insurances, class_name: 'CarVoluntaryInsurance', foreign_key: :company_voluntary_id, dependent: :destroy
   has_many :car_voluntaries, through: :car_voluntary_insurances, source: :car_voluntary
 
