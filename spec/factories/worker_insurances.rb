d6b026e624c665b873b7a883c94f2fc278acf4b5
yusamasaki <masa.aw1208@gmail.com>
2022-02-15 09:17:40 +0900

    spe/factoriesの記述変更

--- spec/factories/worker_insurances.rb
+++ spec/factories/worker_insurances.rb
@@ -1,12 +1,12 @@
 FactoryBot.define do
   factory :worker_insurance do
     association :worker
-    health_insurance_type { 1 }
-    health_insurance_name { 'MyString' }
-    pension_insurance_type { 1 }
-    employment_insurance_type { 1 }
-    employment_insurance_number { '1234' }
-    severance_pay_mutual_aid_type { 1 }
-    severance_pay_mutual_aid_name { 'MyString' }
+    health_insurance_type { rand(4) }
+    sequence(:health_insurance_name) { |n| "health_insurance_name#{n}" }
+    pension_insurance_type { rand(2) }
+    employment_insurance_type { rand(2) }
+    employment_insurance_number { '%.4d' % rand(10000) }
+    severance_pay_mutual_aid_type { rand(3) }
+    sequence(:severance_pay_mutual_aid_name) { |n| "severance_pay_mutual_aid_name#{n}" }
   end
 end
