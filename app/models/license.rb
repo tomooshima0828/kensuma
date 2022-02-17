c029532ea2412fad8b5b1f4b05c76c7eec369060
RumiNakamura <krw.psh2196@gmail.com>
2022-02-17 09:12:49 +0900

    enum定義追加

--- app/models/license.rb
+++ app/models/license.rb
@@ -2,8 +2,11 @@ class License < ApplicationRecord
   has_many :worker_licenses, dependent: :destroy
   has_many :workers, through: :worker_licenses
 
-  # ↓内訳未定のためコメントアウト
-  # enum license_type: {  }
+  enum license_type: {
+    登録基幹技能者: 0, 
+    建設業法: 1, 建築士法: 2, 技術士法: 3, 電気工事士法: 4, 電気事業法: 5,
+    電気通信事業法: 6, 水道法: 7, 消防法: 8, 職業能力開発促進法: 9, その他: 10
+  }
 
   validates :name, presence: true
   validates :license_type, presence: true
