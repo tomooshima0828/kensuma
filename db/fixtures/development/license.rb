246f95437a3961c0b9d0a1cfa322e9b019c64831
RumiNakamura <krw.psh2196@gmail.com>
2022-02-17 10:22:30 +0900

    Licenseテーブルマスタデータ入力完了

---
+++ db/fixtures/development/license.rb
@@ -0,0 +1,749 @@
+License.seed(
+  # ---------- 種別 登録基幹技能者 ----------
+  {
+    name: '登録ALC基幹技能者',
+    license_type: 0
+  },
+  {
+    name: '登録PC基幹技能者',
+    license_type: 0
+  },
+  {
+    name: '登録ウレタン断熱基幹技能者',
+    license_type: 0
+  },
+  {
+    name: '登録エクステリア基幹技能者',
+    license_type: 0
+  },
+  {
+    name: '登録グラウト基幹技能者',
+    license_type: 0
+  },
+  {
+    name: '登録コンクリート圧送基幹技能者',
+    license_type: 0
+  },
+  {
+    name: '登録サッシ・カーテンウォール基幹技能者',
+    license_type: 0
+  },
+  {
+    name: '登録タイル張り基幹技能者',
+    license_type: 0
+  },
+  {
+    name: '登録ダクト基幹技能者',
+    license_type: 0
+  },
+  {
+    name: '登録トンネル基幹技能者',
+    license_type: 0
+  },
+  {
+    name: '登録プレストレスト・コンクリート工事基幹技能者',
+    license_type: 0
+  },
+  {
+    name: '登録圧接基幹技能者',
+    license_type: 0
+  },
+  {
+    name: '登録運動施設基幹技能者',
+    license_type: 0
+  },
+  {
+    name: '登録海上起重基幹技能者',
+    license_type: 0
+  },
+  {
+    name: '登録外壁仕上基幹技能者',
+    license_type: 0
+  },
+  {
+    name: '登録基礎工基幹技能者',
+    license_type: 0
+  },
+  {
+    name: '登録機械土工基幹技能者',
+    license_type: 0
+  },
+  {
+    name: '登録橋梁基幹技能者',
+    license_type: 0
+  },
+  {
+    name: '登録型枠基幹技能者',
+    license_type: 0
+  },
+  {
+    name: '登録建設塗装基幹技能者',
+    license_type: 0
+  },
+  {
+    name: '登録建築大工基幹技能者',
+    license_type: 0
+  },
+  {
+    name: '登録建築板金基幹技能者',
+    license_type: 0
+  },
+  {
+    name: '登録左官基幹技能者',
+    license_type: 0
+  },
+  {
+    name: '登録消火設備基幹技能者',
+    license_type: 0
+  },
+  {
+    name: '登録硝子工事基幹技能者',
+    license_type: 0
+  },
+  {
+    name: '登録切断穿孔基幹技能者',
+    license_type: 0
+  },
+  {
+    name: '登録造園基幹技能者',
+    license_type: 0
+  },
+  {
+    name: '登録鉄筋基幹技能者',
+    license_type: 0
+  },
+  {
+    name: '登録電気工事基幹技能者',
+    license_type: 0
+  },
+  {
+    name: '登録土工基幹技能者',
+    license_type: 0
+  },
+  {
+    name: '登録鳶・土工基幹技能者',
+    license_type: 0
+  },
+  {
+    name: '登録内装仕上工事基幹技能者',
+    license_type: 0
+  },
+  {
+    name: '登録配管基幹技能者',
+    license_type: 0
+  },
+  {
+    name: '登録発破・破砕基幹技能者',
+    license_type: 0
+  },
+  {
+    name: '登録標識・路面標示基幹技能者',
+    license_type: 0
+  },
+  {
+    name: '登録保温保冷基幹技能者',
+    license_type: 0
+  },
+  {
+    name: '登録防水基幹技能者',
+    license_type: 0
+  },
+  {
+    name: '登録冷凍空調基幹技能者',
+    license_type: 0
+  },
+  # ---------- 種別 建設業法 ----------
+  {
+    name: '1級建設機械施工技士',
+    license_type: 1
+  },
+  {
+    name: '2級建設機械施工技士（1種～6種）',
+    license_type: 1
+  },
+  {
+    name: '1級土木施工管理技士',
+    license_type: 1
+  },
+  {
+    name: '2級土木施工管理技士（土木）',
+    license_type: 1
+  },
+  {
+    name: '2級土木施工管理技士（鋼構造物塗装）',
+    license_type: 1
+  },
+  {
+    name: '2級土木施工管理技士（薬液注入）',
+    license_type: 1
+  },
+  {
+    name: '1級建築施工管理技士',
+    license_type: 1
+  },
+  {
+    name: '2級建築施工管理技士（建築）',
+    license_type: 1
+  },
+  {
+    name: '2級建築施工管理技士（躯体）',
+    license_type: 1
+  },
+  {
+    name: '2級建築施工管理技士（仕上げ）',
+    license_type: 1
+  },
+  {
+    name: '1級電気工事施工管理技士',
+    license_type: 1
+  },
+  {
+    name: '2級電気工事施工管理技士',
+    license_type: 1
+  },
+  {
+    name: '1級管工事施工管理技士',
+    license_type: 1
+  },
+  {
+    name: '2級管工事施工管理技士',
+    license_type: 1
+  },
+  {
+    name: '1級造園施工管理技士',
+    license_type: 1
+  },
+  {
+    name: '2級造園施工管理技士',
+    license_type: 1
+  },
+  # ---------- 種別 建築士法 ----------
+  {
+    name: '1級建築士',
+    license_type: 2
+  },
+  {
+    name: '2級建築士',
+    license_type: 2
+  },
+  {
+    name: '木造建築士',
+    license_type: 2
+  },
+  # ---------- 種別 技術士法 ----------
+  {
+    name: '建設・総合技術監理（建設）',
+    license_type: 3
+  },
+  {
+    name: '建設「鋼構造及びコンクリート」総合技術監理（建設「鋼構造物及びコンクリート」）',
+    license_type: 3
+  },
+  {
+    name: '農業「農業土木」 総合技術監理（農業「農業土木」）',
+    license_type: 3
+  },
+  {
+    name: '電気電子/総合技術監理（電気電子）',
+    license_type: 3
+  },
+  {
+    name: '機械・総合技術監理（機械）',
+    license_type: 3
+  },
+  {
+    name: '機械「流体工学」又は「熱工学」総合技術監理（機械「流体工学」又は「熱工学」）',
+    license_type: 3
+  },
+  {
+    name: '上下水道・総合技術監理（上下水道）',
+    license_type: 3
+  },
+  {
+    name: '上下水道「上水道及び工業用水道」 総合技術監理（上下水道「上水道及び工業用水道」）',
+    license_type: 3
+  },
+  {
+    name: '水産土木「水産土木」総合技術監理（水産土木「水産土木」）',
+    license_type: 3
+  },
+  {
+    name: '森林「林業」・総合技術監理（森林「林業」）',
+    license_type: 3
+  },
+  {
+    name: '森林「森林土木」・総合技術監理（森林「森林土木」）',
+    license_type: 3
+  },
+  {
+    name: '衛生工学・総合技術監理（衛生工学）',
+    license_type: 3
+  },
+  {
+    name: '衛生工学「水質管理」総合技術監理（衛生工学「水質管理」）',
+    license_type: 3
+  },
+  {
+    name: '衛生工学「廃棄物管理」 総合技術監理（衛生工学「廃棄物管理」）',
+    license_type: 3
+  },
+  # ---------- 種別 電気工事士法 ----------
+  {
+    name: '1種電気工事士',
+    license_type: 4
+  },
+  {
+    name: '2種電気工事士',
+    license_type: 4
+  },
+  # ---------- 種別 電気事業法 ----------
+  {
+    name: '電気主任技術者1種～3種',
+    license_type: 5
+  },
+  # ---------- 種別 電気通信事業法 ----------
+  {
+    name: '電気通信主任技術者(伝送交換・線路)',
+    license_type: 6
+  },
+  # ---------- 種別 水道法 ----------
+  {
+    name: '給水装置工事主任技術者',
+    license_type: 7
+  },
+  # ---------- 種別 消防法 ----------
+  {
+    name: '消防設備士甲種',
+    license_type: 8
+  },
+  {
+    name: '消防設備士乙種',
+    license_type: 8
+  },
+  # ---------- 種別 職業能力開発促進法 ----------
+  {
+    name: '1級建築大工技能検定',
+    license_type: 9
+  },
+  {
+    name: '2級建築大工技能検定',
+    license_type: 9
+  },
+  {
+    name: '3級建築大工技能検定',
+    license_type: 9
+  },
+  {
+    name: '基礎級建築大工技能検定',
+    license_type: 9
+  },
+  {
+    name: '1級型枠施工技能検定',
+    license_type: 9
+  },
+  {
+    name: '2級型枠施工技能検定',
+    license_type: 9
+  },
+  {
+    name: '3級型枠施工技能検定',
+    license_type: 9
+  },
+  {
+    name: '基礎級型枠施工技能検定',
+    license_type: 9
+  },
+  {
+    name: '1級左官技能検定',
+    license_type: 9
+  },
+  {
+    name: '2級左官技能検定',
+    license_type: 9
+  },
+  {
+    name: '3級左官技能検定',
+    license_type: 9
+  },
+  {
+    name: '基礎級左官技能検定',
+    license_type: 9
+  },
+  {
+    name: '1級とび技能検定',
+    license_type: 9
+  },
+  {
+    name: '2級とび技能検定',
+    license_type: 9
+  },
+  {
+    name: '3級とび技能検定',
+    license_type: 9
+  },
+  {
+    name: '基礎級とび技能検定',
+    license_type: 9
+  },
+  {
+    name: '1級コンクリート圧送施工技能',
+    license_type: 9
+  },
+  {
+    name: '2級コンクリート圧送施工技能',
+    license_type: 9
+  },
+  {
+    name: '3級コンクリート圧送施工技能',
+    license_type: 9
+  },
+  {
+    name: '基礎級コンクリート圧送施工技能検定',
+    license_type: 9
+  },
+  {
+    name: '1級ウェルポイント施工技能検定',
+    license_type: 9
+  },
+  {
+    name: '2級ウェルポイント施工技能検定',
+    license_type: 9
+  },
+  {
+    name: '3級ウェルポイント施工技能検定',
+    license_type: 9
+  },
+  {
+    name: '基礎級ウェルポイント施工技能検定',
+    license_type: 9
+  },
+  {
+    name: '1級冷凍空気調和機器施工技能検定',
+    license_type: 9
+  },
+  {
+    name: '2級冷凍空気調和機器施工技能検定',
+    license_type: 9
+  },
+  {
+    name: '3級冷凍空気調和機器施工技能検定',
+    license_type: 9
+  },
+  {
+    name: '基礎級冷凍空気調和機器施工技能検定',
+    license_type: 9
+  },
+  {
+    name: '1級配管技能検定',
+    license_type: 9
+  },
+  {
+    name: '2級配管技能検定',
+    license_type: 9
+  },
+  {
+    name: '3級配管技能検定',
+    license_type: 9
+  },
+  {
+    name: '基礎級配管技能検定',
+    license_type: 9
+  },
+  {
+    name: '1級建築板金技能検定',
+    license_type: 9
+  },
+  {
+    name: '2級建築板金技能検定',
+    license_type: 9
+  },
+  {
+    name: '3級建築板金技能検定',
+    license_type: 9
+  },
+  {
+    name: '基礎級建築板金技能検定',
+    license_type: 9
+  },
+  {
+    name: '1級タイル張り技能検定',
+    license_type: 9
+  },
+  {
+    name: '2級タイル張り技能検定',
+    license_type: 9
+  },
+  {
+    name: '3級タイル張り技能検定',
+    license_type: 9
+  },
+  {
+    name: '基礎級タイル張り技能検定',
+    license_type: 9
+  },
+  {
+    name: '1級築炉技能検定',
+    license_type: 9
+  },
+  {
+    name: '2級築炉技能検定',
+    license_type: 9
+  },
+  {
+    name: '3級築炉技能検定',
+    license_type: 9
+  },
+  {
+    name: '基礎級築炉技能検定',
+    license_type: 9
+  },
+  {
+    name: '1級ブロック建築技能検定',
+    license_type: 9
+  },
+  {
+    name: '2級ブロック建築技能検定',
+    license_type: 9
+  },
+  {
+    name: '3級ブロック建築技能検定',
+    license_type: 9
+  },
+  {
+    name: '1級石材施工技能検定',
+    license_type: 9
+  },
+  {
+    name: '2級石材施工技能検定',
+    license_type: 9
+  },
+  {
+    name: '3級石材施工技能検定',
+    license_type: 9
+  },
+  {
+    name: '基礎級石材施工技能検定',
+    license_type: 9
+  },
+  {
+    name: '1級鉄工技能検定',
+    license_type: 9
+  },
+  {
+    name: '2級鉄工技能検定',
+    license_type: 9
+  },
+  {
+    name: '3級鉄工技能検定',
+    license_type: 9
+  },
+  {
+    name: '基礎級鉄工技能検定',
+    license_type: 9
+  },
+  {
+    name: '1級鉄筋施工技能検定',
+    license_type: 9
+  },
+  {
+    name: '2級鉄筋施工技能検定',
+    license_type: 9
+  },
+  {
+    name: '3級鉄筋施工技能検定',
+    license_type: 9
+  },
+  {
+    name: '基礎級鉄筋施工技能検定',
+    license_type: 9
+  },
+  {
+    name: '特級電気機器組立て技能検定',
+    license_type: 9
+  },
+  {
+    name: '1級電気機器組立て技能検定',
+    license_type: 9
+  },
+  {
+    name: '2級電気機器組立て技能検定',
+    license_type: 9
+  },
+  {
+    name: '3級電気機器組立て技能検定',
+    license_type: 9
+  },
+  {
+    name: '基礎級電気機器組立て技能検定',
+    license_type: 9
+  },
+  {
+    name: '1級建築板金技能検定',
+    license_type: 9
+  },
+  {
+    name: '2級建築板金技能検定',
+    license_type: 9
+  },
+  {
+    name: '3級建築板金技能検定',
+    license_type: 9
+  },
+  {
+    name: '基礎級建築板金技能検定',
+    license_type: 9
+  },
+  {
+    name: '1級かわらぶき技能検定',
+    license_type: 9
+  },
+  {
+    name: '2級かわらぶき技能検定',
+    license_type: 9
+  },
+  {
+    name: '3級かわらぶき技能検定',
+    license_type: 9
+  },
+  {
+    name: '基礎級かわらぶき技能検定',
+    license_type: 9
+  },
+  {
+    name: '1級ガラス施工技能検定',
+    license_type: 9
+  },
+  {
+    name: '2級ガラス施工技能検定',
+    license_type: 9
+  },
+  {
+    name: '1級塗装技能検定',
+    license_type: 9
+  },
+  {
+    name: '2級塗装技能検定',
+    license_type: 9
+  },
+  {
+    name: '3級塗装技能検定',
+    license_type: 9
+  },
+  {
+    name: '基礎級塗装技能検定',
+    license_type: 9
+  },
+  {
+    name: '路面標示施工技能検定',
+    license_type: 9
+  },
+  {
+    name: '1級畳製作技能検定',
+    license_type: 9
+  },
+  {
+    name: '2級畳製作技能検定',
+    license_type: 9
+  },
+  {
+    name: '1級内装仕上げ施工技能検定',
+    license_type: 9
+  },
+  {
+    name: '2級内装仕上げ施工技能検定',
+    license_type: 9
+  },
+  {
+    name: '3級内装仕上げ施工技能検定',
+    license_type: 9
+  },
+  {
+    name: '基礎級内装仕上げ施工技能検定',
+    license_type: 9
+  },
+  {
+    name: '1級熱絶縁施工技能検定',
+    license_type: 9
+  },
+  {
+    name: '2級熱絶縁施工技能検定',
+    license_type: 9
+  },
+  {
+    name: '3級熱絶縁施工技能検定',
+    license_type: 9
+  },
+  {
+    name: '基礎級熱絶縁施工技能検定',
+    license_type: 9
+  },
+  {
+    name: '1級建具製作技能検定',
+    license_type: 9
+  },
+  {
+    name: '2級建具製作技能検定',
+    license_type: 9
+  },
+  {
+    name: '3級建具製作技能検定',
+    license_type: 9
+  },
+  {
+    name: '基礎級建具製作技能検定',
+    license_type: 9
+  },
+  {
+    name: '1級造園技能検定',
+    license_type: 9
+  },
+  {
+    name: '2級造園技能検定',
+    license_type: 9
+  },
+  {
+    name: '3級造園技能検定',
+    license_type: 9
+  },
+  {
+    name: '1級防水施工技能検定',
+    license_type: 9
+  },
+  {
+    name: '2級防水施工技能検定',
+    license_type: 9
+  },
+  {
+    name: '3級防水施工技能検定',
+    license_type: 9
+  },
+  {
+    name: '1級さく井技能検定',
+    license_type: 9
+  },
+  {
+    name: '2級さく井技能検定',
+    license_type: 9
+  },
+  {
+    name: '3級さく井技能検定',
+    license_type: 9
+  },
+  {
+    name: '基礎級さく井技能検定',
+    license_type: 9
+  },
+  # ---------- 種別 その他 ----------
+  {
+    name: '地すべり防止工事士',
+    license_type: 9
+  },
+  {
+    name: '建築設備士',
+    license_type: 9
+  },
+  {
+    name: '1級計装士',
+    license_type: 9
+  }
+)
