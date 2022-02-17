f6fb4275292f3bc6d3680b022e68ccbaa97d65a0
Tadakiyo-Hirano <d85nus03@gmail.com>
2022-02-17 11:43:54 +0900

    バリデーション設定

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,10 @@
 class Order < ApplicationRecord
+  belongs_to :business
+
+  validates :status, presence: true
+  validates :site_uu_id, presence: true
+  validates :site_name, presence: true
+  validates :order_name, presence: true
+  validates :order_post_code, presence: true
+  validates :order_address, presence: true
 end
