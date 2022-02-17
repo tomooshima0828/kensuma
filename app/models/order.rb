351f5e9fdb2e61afa6df8b7bbd0188a0bf9cfeae
Tadakiyo-Hirano <d85nus03@gmail.com>
2022-02-17 13:16:21 +0900

    enum設定

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,6 @@
 class Order < ApplicationRecord
   belongs_to :business
+  before_create -> { self.site_uu_id = SecureRandom.uuid }
 
   validates :status, presence: true
   validates :site_uu_id, presence: true
@@ -7,4 +8,6 @@ class Order < ApplicationRecord
   validates :order_name, presence: true
   validates :order_post_code, presence: true
   validates :order_address, presence: true
+
+  enum status: { created: 0, completed: 1 }
 end
