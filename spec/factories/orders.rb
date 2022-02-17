1a42e23115db54161b677f4c9d72cf6420a42abe
Tadakiyo-Hirano <d85nus03@gmail.com>
2022-02-17 16:50:21 +0900

    factory bot修正

--- spec/factories/orders.rb
+++ spec/factories/orders.rb
@@ -2,10 +2,10 @@ FactoryBot.define do
   factory :order do
     status { 0 }
     site_uu_id { SecureRandom.uuid }
-    sequence(:site_name) { |n| "site#{n}" }
-    sequence(:order_name) { |n| "name#{n}" }
+    sequence(:site_name) { |n| "site_name#{n}" }
+    sequence(:order_name) { |n| "order_name#{n}" }
     order_post_code { '1000014' }
-    sequence(:order_address) { |n| "address#{n}" }
+    sequence(:order_address) { |n| "order_address#{n}" }
     business
   end
 end
