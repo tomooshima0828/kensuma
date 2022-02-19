219968deec790d08cc075a93ff833527154c251f
Tadakiyo-Hirano <d85nus03@gmaul.com>
2022-02-19 11:18:17 +0900

    site_uu_idバリデーション修正

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,7 +4,6 @@ class Order < ApplicationRecord
   enum status: { created: 0, completed: 1 }
 
   validates :status, presence: true
-  validates :site_uu_id, presence: true
   validates :site_name, presence: true
   validates :order_name, presence: true
   validates :order_post_code, presence: true, format: { with: /\A\^\d{5}$|^\d{7}\z/ }
