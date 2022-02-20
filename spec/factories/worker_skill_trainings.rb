e2f3bf8f70d6eb3a2893c346c8afb482a494f80c
RumiNakamura <krw.psh2196@gmail.com>
2022-02-20 21:03:34 +0900

    factorybotにアソシエーションの設定を追加

--- spec/factories/worker_skill_trainings.rb
+++ spec/factories/worker_skill_trainings.rb
@@ -2,5 +2,7 @@ FactoryBot.define do
   factory :worker_skill_training do
     got_on { '2022-02-12' }
     images { '' }
+    worker
+    skill_training
   end
 end
--- spec/factories/worker_special_educations.rb
+++ spec/factories/worker_special_educations.rb
@@ -2,5 +2,7 @@ FactoryBot.define do
   factory :worker_special_education do
     got_on { '2022-02-12' }
     images { '' }
+    worker
+    special_education
   end
 end
