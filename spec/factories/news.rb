d303b5535c3741dd34643cfbe97094dfaf4af04a
tomooshima0828 <tomo.oshima0828@gmail.com>
2022-02-20 19:41:53 +0900

    Modify News model and model spec

--- spec/factories/news.rb
+++ spec/factories/news.rb
@@ -2,8 +2,7 @@ FactoryBot.define do
   factory :news do
     title { 'MyString' }
     context { 'MyText' }
-
-    status { 1 }
     delivered_at { '2022-02-15 07:59:49' }
+    status { 1 }
   end
 end
--- spec/models/news_spec.rb
+++ spec/models/news_spec.rb
@@ -13,5 +13,35 @@ RSpec.describe News, type: :model do
     it 'バリデーションが通ること' do
       expect(subject).to be_valid
     end
+
+    describe '#title' do
+     context '存在しない場合' do
+       before :each do
+         subject.title = nil
+       end
+       it 'バリデーションに落ちること' do
+         expect(subject).to be_invalid
+       end
+       it 'バリデーションのエラーが正しいこと' do
+         subject.valid?
+         expect(subject.errors.full_messages).to include('Titleを入力してください')
+       end
+     end
+    end
+
+    describe '#status' do
+      context '存在しない場合' do
+        before :each do
+          subject.status = nil
+        end
+        it 'バリデーションに落ちること' do
+          expect(subject).to be_invalid
+        end
+        it 'バリデーションのエラーが正しいこと' do
+          subject.valid?
+          expect(subject.errors.full_messages).to include("Statusを入力してください")
+        end
+      end
+    end
   end
 end
