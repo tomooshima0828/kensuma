6d6e1d5b4b45d40a9abeb3954e903f6e67e8fe6b
yusamasaki <masa.aw1208@gmail.com>
2022-02-22 07:08:34 +0900

    table再修正

--- db/migrate/20220221215957_add_culumns_in_car_voluntary_insurances.rb
+++ db/migrate/20220221215957_add_culumns_in_car_voluntary_insurances.rb
@@ -1,4 +1,7 @@
 class AddCulumnsInCarVoluntaryInsurances < ActiveRecord::Migration[6.1]
   def change
+    add_column :car_voluntary_insurances, :voluntary_securities_number, :string
+    add_column :car_voluntary_insurances, :voluntary_insurance_start_on, :date
+    add_column :car_voluntary_insurances, :voluntary_insurance_end_on, :date
   end
 end
