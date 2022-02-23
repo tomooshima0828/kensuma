2b8e630302a82172af53436a754da43908e8ba27
tomooshima0828 <tomo.oshima0828@gmail.com>
2022-02-24 01:23:39 +0900

    Modify and complete to fulfill minimum requirements for user news CRUD

--- spec/requests/news_spec.rb
+++ spec/requests/news_spec.rb
@@ -2,16 +2,6 @@ require 'rails_helper'
 
 RSpec.describe 'News', type: :request do
   describe 'GET /index' do
-    it 'returns http success' do
-      get '/news/index'
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe 'GET /show' do
-    it 'returns http success' do
-      get '/news/show'
-      expect(response).to have_http_status(:success)
-    end
+    pending "add some examples (or delete) #{__FILE__}"
   end
 end
