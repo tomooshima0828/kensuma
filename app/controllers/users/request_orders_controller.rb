326011663deff75e853ae56459a0f316f0e46a7d
Tadakiyo-Hirano <d85nus03@gmail.com>
2022-03-03 14:45:35 +0900

    RequestOrders view作成

--- app/controllers/users/request_orders_controller.rb
+++ app/controllers/users/request_orders_controller.rb
@@ -1,9 +1,11 @@
 module Users
   class RequestOrdersController < Users::Base
     def index
+      @request_orders = RequestOrder.where(business: current_business)
     end
 
     def show
+      @request_order = current_business.request_orders.find(params[:id])
     end
   end
 end
