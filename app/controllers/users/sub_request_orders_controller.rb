11e76ea98f0256e3db571d58893de5007c2dae6f
yusamasaki <masa.aw1208@gmail.com>
2022-03-06 11:15:01 +0900

    下請け発注依頼の新規作成機能追加

--- app/controllers/users/sub_request_orders_controller.rb
+++ app/controllers/users/sub_request_orders_controller.rb
@@ -1,11 +1,34 @@
 module Users
   class SubRequestOrdersController < Users::Base
+    before_action :set_request_order
+
     def index; end
 
-    def new; end
+    def new
+      @sub_request_order = @request_order.order.request_orders.build
+      @businesses = Business.where.not(id: current_business)
+    end
 
-    def create; end
+    def create
+      @sub_request_order = @request_order.order.request_orders.build(sub_request_order_params)
+      if @sub_request_order.save
+        flash[:success] = '下請けへの発注依頼を作成しました'
+        redirect_to users_request_order_url(@request_order)
+      else
+        render :new
+      end
+    end
 
     def update; end
+
+    private
+
+    def set_request_order
+      @request_order = current_business.request_orders.find_by(uuid: params[:request_order_uuid])
+    end
+
+    def sub_request_order_params
+      params.require(:request_order).permit(:business_id, :parent_id)
+    end
   end
 end
