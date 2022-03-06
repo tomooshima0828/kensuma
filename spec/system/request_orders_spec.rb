235ad610c57a7f5417b457bfff9eac39b8cc3be3
yusamasaki <masa.aw1208@gmail.com>
2022-03-06 18:20:15 +0900

    systemspec修正

--- spec/system/request_orders_spec.rb
+++ spec/system/request_orders_spec.rb
@@ -17,7 +17,7 @@ RSpec.describe 'RequestOrders', type: :system do
       click_button 'ログイン'
     end
 
-    context 'ログイン後' do
+    context '発注依頼' do
       it '発注一覧画面へ遷移できること' do
         visit users_request_orders_path
         expect(page).to have_content '発注依頼一覧'
@@ -28,6 +28,23 @@ RSpec.describe 'RequestOrders', type: :system do
         expect(page).to have_content '発注依頼詳細'
         expect(page).to have_content request_order.status_i18n
         expect(page).to have_content request_order.business.name
+      end
+    end
+
+    context '下請け発注依頼' do
+      it '下請け発注依頼登録画面' do
+        user = create(:user)
+        business = create(:business, user: user, name: 'test')
+
+        visit new_users_request_order_sub_request_order_path(request_order)
+        expect(page).to have_content '下請け発注依頼登録'
+
+        select business.name, from: 'request_order_business_id'
+
+        click_button '登録'
+
+        # 登録後、発注依頼詳細画面へ
+        expect(page).to have_content '発注依頼詳細'
       end
     end
   end
