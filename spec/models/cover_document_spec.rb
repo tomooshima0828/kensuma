c1da380b34f5b700e8f0bb026482db3e5f106ef1
tomooshima0828 <tomo.oshima0828@gmail.com>
2022-03-09 17:48:01 +0900

    test passed

--- spec/models/cover_document_spec.rb
+++ spec/models/cover_document_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe CoverDocument, type: :model do
   let(:document) { build(:document) }
   let(:cover_document) { build(:cover_document, document: document) }
+
   describe 'documentとcover_documentの生成と保存' do
     it 'documentとcover_documentが保存されること' do
       cover_document.save!
--- spec/models/document_spec.rb
+++ spec/models/document_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Document, type: :model do
   let(:document) { build(:document) }
+
   describe 'documentの生成と保存' do
     it 'documentが保存されること' do
       document.save!
