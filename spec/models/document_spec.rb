c03801120c944b06d0aad00d2b368fc62d0e42dd
tomooshima0828 <tomo.oshima0828@gmail.com>
2022-03-09 17:22:01 +0900

    modify spec files

--- spec/models/document_spec.rb
+++ spec/models/document_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Document, type: :model do
   let(:document) { build(:document) }
-  describe '書類のバリデーション' do
-    it '書類のバリデーションが通り保存されること' do
+  describe 'documentの生成と保存' do
+    it 'documentが保存されること' do
       document.save!
       expect(subject).to be_valid
     end
