abc968aeddfabfc1a65c0d191defb5f4bb9d9cb0
yusamasaki <masa.aw1208@gmail.com>
2022-03-09 21:52:54 +0900

    create処理修正

--- app/controllers/users/sub_request_orders_controller.rb
+++ app/controllers/users/sub_request_orders_controller.rb
@@ -10,23 +10,16 @@ module Users
     end
 
     def create
-      # params[:business_ids]の最初の空欄部を削除
-      params[:business_ids].delete('')
-      if params[:business_ids].present?
-        save_count = no_save_count = 0 # save_countとno_save_countに0を代入
-        params[:business_ids].each do |business_id|
-          sub_request_order = @request_order.order.request_orders.build(business_id: business_id.to_i, parent_id: @request_order.id)
-          # もしsub_request_orderが何らかの理由でsaveできていなかった場合no_save_countに1足される処理。
-          sub_request_order.save ? (save_count += 1) : (no_save_count += 1)
+      ActiveRecord::Base.transaction do
+        params[:business_ids].each do |select_business|
+          @request_order.order.request_orders.create!(business_id: select_business, parent_id: @request_order.id)
         end
-        flash[:success] = "下請けへの発注依頼を#{save_count}件作成しました"
-        # もしsub_request_orderが何らかの理由でsaveできていなかった場合のフラッシュメッセージ
-        flash[:danger] = "下請けへの発注依頼作成に#{no_save_count}件失敗しました" if no_save_count >= 1
+        flash[:success] = "#{params[:business_ids].count}件の発注依頼を作成しました。"
         redirect_to users_request_order_url(@request_order)
-      else
-        flash[:danger] = '下請けへの発注依頼に失敗しました。'
-        redirect_to new_users_request_order_sub_request_order_url(@request_order)
       end
+    rescue ActiveRecord::RecordInvalid
+      flash[:danger] = '発注依頼に失敗しました。再度作成してください。'
+      redirect_to new_users_request_order_sub_request_order_path
     end
 
     def update; end
