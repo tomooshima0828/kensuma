1f5371a75ec8d86003468e4fc6bfe6fed17b16bf
shotaimai66 <changemymind6@gmail.com>
2022-03-14 09:45:50 +0900

    fix: seed_fu

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,3 @@
 # frozen_string_literal: true
 
-# User
-unless User.exists?
-  1.times do |i|
-    user = User.new(
-      email: "test_user#{i}@gmail.com", # sample: test_user1@gmail.com
-      name: "テストuser#{i}",
-      password: 'password'
-    )
-
-    user.skip_confirmation! # deviseの確認メールをスキップ
-    user.save!
-  end
-end
-
 SeedFu.seed
