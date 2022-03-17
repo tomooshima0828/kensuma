500c4776b3ed7d3bef1363a134f3f9b3ca9252ba
tomooshima0828 <tomo.oshima0828@gmail.com>
2022-03-17 13:48:31 +0900

    set validation to worker_medical model and set translation words to model_ja.yml

--- app/models/worker.rb
+++ app/models/worker.rb
@@ -1,7 +1,7 @@
 class Worker < ApplicationRecord
   belongs_to :business
   has_one :worker_insurance, dependent: :destroy
-  has_one :worker_medical
+  has_one :worker_medical, dependent: :destroy
 
   has_many :worker_licenses, dependent: :destroy
   has_many :licenses, through: :worker_licenses
--- app/models/worker_medical.rb
+++ app/models/worker_medical.rb
@@ -1,3 +1,7 @@
 class WorkerMedical < ApplicationRecord
   belongs_to :worker # Workerにhas_oneを設定
+
+  validates :medical_examination_on, presence: true
+  validates :max_blood_pressure, presence: true
+  validates :min_blood_pressure, presence: true
 end
