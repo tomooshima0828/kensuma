3603ff5840bd752ec247a98651fc09881d459106
tomooshima0828 <tomo.oshima0828@gmail.com>
2022-03-21 21:24:41 +0900

    tentatively save

--- app/models/worker_medical.rb
+++ app/models/worker_medical.rb
@@ -1,7 +1,6 @@
 class WorkerMedical < ApplicationRecord
   belongs_to :worker # Workerにhas_oneを設定
   has_many :worker_examinations, dependent: :destroy # 中間テーブル
-  has_many :special_medical_examinations, through: :worker_examinations
 
   validates :medical_examination_on, presence: true
   validates :max_blood_pressure, presence: true
