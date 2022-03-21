e2771afcb3edb5ac12b95702ff71420977322d9f
tomooshima0828 <tomo.oshima0828@gmail.com>
2022-03-21 14:34:17 +0900

    Modify model relationships

--- app/models/worker_examination.rb
+++ app/models/worker_examination.rb
@@ -1,5 +1,6 @@
 class WorkerExamination < ApplicationRecord
   belongs_to :worker_medical
+  belongs_to :worker, through: :worker_medical
   belongs_to :special_medical_examination
 
   validates :got_on, presence: true
--- app/models/worker_medical.rb
+++ app/models/worker_medical.rb
@@ -1,6 +1,7 @@
 class WorkerMedical < ApplicationRecord
   belongs_to :worker # Workerにhas_oneを設定
-  has_many :worker_examinations, dependent: :destroy
+  has_many :worker_examinations, dependent: :destroy # 中間テーブル
+  has_many :special_medical_examinations, through: :worker_examinations
 
   validates :medical_examination_on, presence: true
   validates :max_blood_pressure, presence: true
