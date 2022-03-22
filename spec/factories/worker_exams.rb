6d7f6ba21d2cad1b8a65b89a23cafa05488b8fd8
tomooshima0828 <tomo.oshima0828@gmail.com>
2022-03-22 18:20:59 +0900

    Modify specs

--- spec/factories/worker_exams.rb
+++ spec/factories/worker_exams.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
-  factory :worker_examination do
+  factory :worker_exam do
     worker_medical { nil }
-    special_medical_examination { nil }
+    special_med_exam { nil }
     got_on { "2022-03-17" }
     images { "" }
   end
--- spec/models/special_med_exam_spec.rb
+++ spec/models/special_med_exam_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
-RSpec.describe SpecialMedicalExamination, type: :model do
+RSpec.describe SpecialMedExam, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
 end
