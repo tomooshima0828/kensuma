1ddb7656d304a6506cce01e2aeccdf63cf21b868
Tadakiyo-Hirano <d85nus03@gmail.com>
2022-03-25 17:32:15 +0900

    pdf画面レイアウト整形

--- spec/requests/second_documents_spec.rb
+++ spec/requests/second_documents_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
-RSpec.describe "SecondDocuments", type: :request do
-  describe "GET /index" do
+RSpec.describe 'SecondDocuments', type: :request do
+  describe 'GET /index' do
     pending "add some examples (or delete) #{__FILE__}"
   end
 end
