3bdd6c35e24f4dec85c80d1af6ec8b5c27af4184
Tadakiyo-Hirano <d85nus03@gmail.com>
2022-03-28 15:22:23 +0900

    Documentモデル contentカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_22_065548) do
+ActiveRecord::Schema.define(version: 2022_03_28_051757) do
 
   create_table "active_admin_comments", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
     t.string "namespace"
@@ -137,6 +137,7 @@ ActiveRecord::Schema.define(version: 2022_03_22_065548) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "uuid", null: false
+    t.json "content"
   end
 
   create_table "licenses", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
