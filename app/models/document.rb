243fbb55d81ce50895fccfa64c98c18187b5dd35
Tadakiyo-Hirano <d85nus03@gmail.com>
2022-03-28 15:49:22 +0900

    enum設定

--- app/models/document.rb
+++ app/models/document.rb
@@ -5,6 +5,18 @@ class Document < ApplicationRecord
 
   before_create -> { self.uuid = SecureRandom.uuid }
 
+  enum document_type: {
+    cover_document: 0,
+    table_of_contents_document: 1,
+    doc_2nd: 2,
+    doc_3rd: 3,
+    doc_4th: 4,
+    doc_5th: 5,
+    doc_6th: 6,
+    doc_7th: 7,
+    doc_8th: 8,
+  }
+
   def to_param
     uuid
   end
