560ffd56335cb7909c03c1bec5321cdba3a94ce5
HARAYUKA <tukituki442@icloud.com>
2022-04-03 22:31:53 +0900

    事業所テストデータ・userデータ増やす

--- db/fixtures/development/01_user.rb
+++ db/fixtures/development/01_user.rb
@@ -14,6 +14,42 @@ User.seed(:id,
     name: 'テストuser2',
     password: 'password',
     password_confirmation: 'password',
+    role: 'admin',
+    confirmed_at: Time.now
+  },
+  {
+    id: 3,
+    email: 'test_user3@gmail.com',
+    name: 'テストuser3',
+    password: 'password',
+    password_confirmation: 'password',
+    role: 'admin',
+    confirmed_at: Time.now
+  },
+  {
+    id: 4,
+    email: 'test_user4@gmail.com',
+    name: 'テストuser4',
+    password: 'password',
+    password_confirmation: 'password',
+    role: 'admin',
+    confirmed_at: Time.now
+  },
+  {
+    id: 5,
+    email: 'test_user5@gmail.com',
+    name: 'テストuser5',
+    password: 'password',
+    password_confirmation: 'password',
+    role: 'admin',
+    confirmed_at: Time.now
+  },
+  {
+    id: 6,
+    email: 'general_user1@gmail.com',
+    name: '一般user1',
+    password: 'password',
+    password_confirmation: 'password',
     role: 'general',
     confirmed_at: Time.now
   }
