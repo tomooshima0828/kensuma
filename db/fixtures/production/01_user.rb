988b4b1dce2e3a255c583bba5d920b4f6eb2e987
HARAYUKA <tukituki442@icloud.com>
2022-04-06 22:37:58 +0900

    Userのseed修正

--- db/fixtures/production/01_user.rb
+++ db/fixtures/production/01_user.rb
@@ -1,56 +1,26 @@
-User.seed(:id,
-  {
-    id: 1,
-    email: 'test_user1@gmail.com',
-    name: 'テストuser1',
-    password: 'password',
-    password_confirmation: 'password',
-    role: 'admin',
-    confirmed_at: Time.now
-  },
-  {
-    id: 2,
-    email: 'test_user2@gmail.com',
-    name: 'テストuser2',
-    password: 'password',
-    password_confirmation: 'password',
-    role: 'admin',
-    confirmed_at: Time.now
-  },
-  {
-    id: 3,
-    email: 'test_user3@gmail.com',
-    name: 'テストuser3',
-    password: 'password',
-    password_confirmation: 'password',
-    role: 'admin',
-    confirmed_at: Time.now
-  },
-  {
-    id: 4,
-    email: 'test_user4@gmail.com',
-    name: 'テストuser4',
-    password: 'password',
-    password_confirmation: 'password',
-    role: 'admin',
-    confirmed_at: Time.now
-  },
-  {
-    id: 5,
-    email: 'test_user5@gmail.com',
-    name: 'テストuser5',
-    password: 'password',
-    password_confirmation: 'password',
-    role: 'admin',
-    confirmed_at: Time.now
-  },
-  {
-    id: 6,
-    email: 'general_user1@gmail.com',
-    name: '一般user1',
-    password: 'password',
-    password_confirmation: 'password',
-    role: 'general',
-    confirmed_at: Time.now
-  }
-)
+5.times do |n|
+  User.seed(:id,
+    {
+      email: "test_user#{n+1}@gmail.com",
+      name: "テストuser#{n+1}",
+      password: 'password',
+      password_confirmation: 'password',
+      role: 'admin',
+      confirmed_at: Time.now
+    }
+  )
+end
+
+3.times do |n|
+  User.seed(:id,
+    {
+      email: "general_user#{n+1}@gmail.com",
+      name: "一般user#{n+1}",
+      password: 'password',
+      password_confirmation: 'password',
+      role: 'general',
+      confirmed_at: Time.now,
+      admin_user_id: 1
+    }
+  )
+end
