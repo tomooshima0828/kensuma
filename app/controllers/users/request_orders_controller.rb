27c79b379f378a6355107d755be27b2b937bb1f5
HARAYUKA <tukituki442@icloud.com>
2022-04-14 21:47:11 +0900

    下請け一覧表示

--- app/controllers/users/request_orders_controller.rb
+++ app/controllers/users/request_orders_controller.rb
@@ -6,6 +6,7 @@ module Users
 
     def show
       @request_order = current_business.request_orders.find_by(uuid: params[:uuid])
+      @sub_request_orders = @request_order.order.request_orders.where.not(business_id: @request_order.id, parent_id: nil)
     end
   end
 end
