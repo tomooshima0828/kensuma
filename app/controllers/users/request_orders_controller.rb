c4ab501d3dce6c9067e2a521285841e352d47fde
HARAYUKA <tukituki442@icloud.com>
2022-04-15 22:10:26 +0900

    コントローラ修正

--- app/controllers/users/request_orders_controller.rb
+++ app/controllers/users/request_orders_controller.rb
@@ -6,7 +6,7 @@ module Users
 
     def show
       @request_order = current_business.request_orders.find_by(uuid: params[:uuid])
-      @sub_request_orders = @request_order.order.request_orders.where(parent_id: @request_order.id)
+      @sub_request_orders = @request_order.children
     end
   end
 end
