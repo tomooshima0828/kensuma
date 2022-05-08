54de7208b5abbbba7dbeec698619c6684bb79486
Yoshi_3line <ny3line@gmail.com>
2022-05-08 22:17:09 +0900

    お知らせページ再修正

--- db/fixtures/development/17_news.rb
+++ db/fixtures/development/17_news.rb
@@ -4,7 +4,7 @@
       id: n+1,
       title: "お知らせ-#{n+1}",
       content: "改行テスト-#{n+1}\r\n改行\r再度改行\n最終行".gsub(/\R/, "\n"),
-      delivered_at: DateTime.now.yesterday,
+      delivered_at: DateTime.now - (n+1),
       status: 'published'
     }
   )
