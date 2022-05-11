0c5020c726be830f7768a20254992e0c26d07221
HARAYUKA <tukituki442@icloud.com>
2022-05-11 13:22:38 +0900

    rubocop修正

--- app/uploaders/base.rb
+++ app/uploaders/base.rb
@@ -34,8 +34,8 @@ class Base < CarrierWave::Uploader::Base
       model.instance_variable_set(media_original_filenames_var, {})
     end
 
-    unless model.instance_variable_get(media_original_filenames_var).map{|k,v| k }.include? original_filename.to_sym
-      new_value = model.instance_variable_get(media_original_filenames_var).merge({"#{original_filename}": SecureRandom.uuid})
+    unless model.instance_variable_get(media_original_filenames_var).map { |k, _v| k }.include? original_filename.to_sym
+      new_value = model.instance_variable_get(media_original_filenames_var).merge({ "#{original_filename}": SecureRandom.uuid })
       model.instance_variable_set(media_original_filenames_var, new_value)
     end
 
