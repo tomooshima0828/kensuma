6af03d8d14893bfe453b711ce6decc4a92769b95
HARAYUKA <tukituki442@icloud.com>
2022-05-11 12:04:53 +0900

    画像upload設定を複数投稿対応に変更

--- app/uploaders/base.rb
+++ app/uploaders/base.rb
@@ -22,13 +22,23 @@ class Base < CarrierWave::Uploader::Base
   end
 
   def filename
-    "#{secure_token}_#{original_filename}" if original_filename.present?
+    "#{secure_token}.#{file.extension}" if original_filename.present?
   end
 
   protected
 
   def secure_token
-    var = :"@#{mounted_as}_secure_token"
-    model.instance_variable_get(var) or model.instance_variable_set(var, SecureRandom.uuid)
+    media_original_filenames_var = :"@#{mounted_as}_original_filenames"
+
+    unless model.instance_variable_get(media_original_filenames_var)
+      model.instance_variable_set(media_original_filenames_var, {})
+    end
+
+    unless model.instance_variable_get(media_original_filenames_var).map{|k,v| k }.include? original_filename.to_sym
+      new_value = model.instance_variable_get(media_original_filenames_var).merge({"#{original_filename}": SecureRandom.uuid})
+      model.instance_variable_set(media_original_filenames_var, new_value)
+    end
+
+    model.instance_variable_get(media_original_filenames_var)[original_filename.to_sym]
   end
 end
