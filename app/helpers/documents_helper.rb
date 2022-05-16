e2554a44f87c329a1d771a29898680c7f4137f19
Tadakiyo-Hirano <d85nus03@gmail.com>
2022-05-16 17:39:22 +0900

    json結合

--- app/helpers/documents_helper.rb
+++ app/helpers/documents_helper.rb
@@ -3,8 +3,20 @@ module DocumentsHelper
     worker[1] + 1 unless worker.nil?
   end
 
+  # contentの作業員jsonデータ workerモデルのカラム表示
   def worker_column(column, worker)
     # @document.content['worker'][column][worker[1]] unless worker.nil?
     JSON.parse(worker[0])[column] unless worker.nil?
   end
+
+  # contentの作業員jsonデータ worker_medicalモデルのカラム表示
+  def worker_attribute_column(model, column, worker)
+    # JSON.parse(w1[0])["worker_medical"]["special_med_exam_on"] unless w1.nil?
+    JSON.parse(worker[0])[model][column] unless worker.nil?
+  end
+
+  # def worker_attribute(model, column, worker_medical)
+  #   # JSON.parse(w1[0])["worker_medical"]["special_med_exam_on"] unless w1.nil?
+  #   JSON.parse(worker_medical[0])[model][column] unless worker_medical.nil?
+  # end
 end
